c022ce7c9cbc8f5419e0a9a80ec04ddc581c63a9
Geming400 <Geming400@gmail.com>
2025-02-04 18:48:45 +0100

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,3 +7,12 @@ int levelthumbs::getQualityMultiplier(){
     return 4 / CCDirector::sharedDirector()->getContentScaleFactor();
 }
 
+std::string levelthumbs::getBaseUrl(){
+    std::string baseURL = Mod::get()->getSettingValue<std::string>("string-setting-example");
+	if (baseURL == "")
+	    baseURL = "https://raw.githubusercontent.com/cdc-sys/level-thumbnails/main/thumbs";
+	if (baseURL.ends_with("/"))
+	    baseURL.pop_back(); // remove the last character (aka: as "/")
+
+    return baseURL;
+}
